029bcbbd0f66c6e2034cd113680201a0719100ca
LindaShar <124031878+LindaShar@users.noreply.github.com>
2023-01-31 09:55:13 +0200

    02 commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# initializing a list
+my_list = [5, 2, 1, 4, 5, 2, 3, 4, 5, 1]
+
+# removing duplicates
+my_list = list(set(my_list))
+
+# reversing the order
+my_list = my_list[::-1]
+
+# arranging list by changing pointers
+my_list.sort()
+
+# arranging list by changing item values
+my_list = sorted(my_list)
+
+print(my_list)
